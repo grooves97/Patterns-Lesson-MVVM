1d485c553c565a8dfabe24ac4e40bbb27428d193
kiwagu <kiwagu@gmail..com>
2019-06-21 02:12:05 +0600

    ADDED: step 4

--- Step/Model.cs
+++ Step/Model.cs
@@ -1,6 +1,15 @@
-﻿namespace MvvmByStep.Step
+﻿using System;
+
+namespace MvvmByStep.Step
 {
 	public class Model
 	{
+		public string Name { get; set; }
+
+		public string Address { get; set; }
+
+		public bool IsMale { get; set; }
+
+		public DateTime BirthDate { get; set; }
 	}
 }
