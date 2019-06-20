22aad1e3bca3c79f25baf92d53b6140b45299ae8
kiwagu <kiwagu@gmail..com>
2019-06-21 02:23:53 +0600

    ADDED: step 6

---
+++ Step/NegatedBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MvvmByStep.Step
+{
+	public class NegatedBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool)
+			{
+				return !((bool) value);
+			}
+			else
+			{
+				return Binding.DoNothing;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return this.Convert(value, targetType, parameter, culture);
+		}
+	}
+}
